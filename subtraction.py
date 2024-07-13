8071ad95020582493f9ad8b30a47daca31c290f5
Mariam-Shammakh <rinmariam7@gmail.com>
2024-07-13 20:51:30 +0300

    subtraction function for tow numbers

---
+++ subtraction.py
@@ -0,0 +1,7 @@
+def subtraction(num1, num2):
+    return num1 - num2
+
+
+n1 = int(input("Enter 1st number: "))
+n2 = int(input("Enter 2nd number: "))
+print(f"the subtraction of {n1} - {n2} = {subtraction(n1,n2)}")
