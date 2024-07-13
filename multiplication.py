34507f799e41430f21cd43b6721c6396715faddd
Mariam-Shammakh <rinmariam7@gmail.com>
2024-07-13 21:08:12 +0300

    multiplication function for two numbers

---
+++ multiplication.py
@@ -0,0 +1,7 @@
+def multiplication(num1, num2):
+    return num1 * num2
+
+
+n1 = int(input("Enter 1st number: "))
+n2 = int(input("Enter 2nd number: "))
+print(f"the multiplication of {n1} * {n2} = {multiplication(n1,n2)}")
